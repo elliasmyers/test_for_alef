40adda85ea43e930a59521f15eac0b027884d9a8
elliotmyers <elliotmyers47@gmail.com>
2023-07-26 15:06:04 +0300

    Initial Commit

---
+++ Alef/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alef
+//
+//  Created by Raiden Yamato on 26.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
